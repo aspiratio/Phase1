2cf092cb7584d44dbd2ca8cb3ba7112e1574e22f
aspiratio <aspiratio-eng@yahoo.co.jp>
2021-04-17 22:51:10 +0900

    サイコロアプリ作成中

---
+++ js-easy/dice/main.js
@@ -0,0 +1,13 @@
+// サイコロをふるをクリック
+// 繰り返し処理を開始
+// 3秒後に終了
+
+const startBtn = document.getElementById("diceBtn");
+const body = document.getElementsByTagName("body");
+const dice = document.createElement("img");
+
+body.appendChild(dice);
+dice.style.width = "100%";
+dice.style.height = "100%";
+
+let num = 1;
